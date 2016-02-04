2f42b4713faadd82635a4052baabf33178b7d6ad
Nurbol Alpysbayev <nurbol@intelit.kz>
2016-02-04 19:49:37 +0600

    Changes...

---
+++ src/Page.php
@@ -0,0 +1,90 @@
+<?php namespace Intelitkz\Laraveltools;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * App\Models\Page
+ *
+ * @property integer $id
+ * @property string $method
+ * @property string $name
+ * @property string $uses
+ * @property string $uri_prefix
+ * @property string $uri_suffix
+ * @property string $parent
+ * @property boolean $in_menu
+ * @property \Carbon\Carbon $deleted_at
+ */
+class Page extends \Eloquent
+{
+	use SoftDeletes;
+
+	/**
+	 * The attributes that should be mutated to dates.
+	 *
+	 * @var array
+	 */
+	protected $dates = ['deleted_at'];
+
+	public function getUri()
+	{
+		if ($this->custom_uri)
+			return $this->custom_uri;
+
+		$uri = $this->method.ucfirst($this->name);
+		$uri = $this->uri_prefix.$uri.$this->uri_suffix;
+
+		return $uri;
+	}
+
+	public function getName()
+	{
+		return $this->method.ucfirst($this->name);
+	}
+
+	public function getTitle()
+	{
+		return trans('pages.'.$this->name);
+	}
+
+	public static function  setRoutes()
+	{
+		foreach (Page::all() as $page)
+		{
+			switch($page->method)
+			{
+				case 'get':
+					\Route::get($page->getUri(), ['as' => $page->getName(), 'uses' => $page->uses]);
+					break;
+				case 'post':
+					\Route::post($page->getUri(), ['as' => $page->getName(), 'uses' => $page->uses]);
+					break;
+				case 'patch':
+					\Route::patch($page->getUri(), ['as' => $page->getName(), 'uses' => $page->uses]);
+					break;
+				case 'delete':
+					\Route::patch($page->getUri(), ['as' => $page->getName(), 'uses' => $page->uses]);
+			}
+		}
+	}
+
+	public static function setBreadcrumbs()
+	{
+		/**
+		 * @var $page Page
+		 */
+		foreach (Page::all() as $page)
+		{
+			\Breadcrumbs::register($page->getName(), function($breadcrumbs) use ($page)
+			{
+				$page->parent
+					? $breadcrumbs->parent($page->parent)
+					: ($page->name != 'home' && $breadcrumbs->parent('home'));
+
+				$breadcrumbs->push(trans('pages.'.$page->name), $page->getUri());
+			});
+		}
+	}
+
+
+}
---
+++ src/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php namespace Intelitkz\Laraveltools;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider {
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+	}
+
+
+}
